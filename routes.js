706d518dc95ba74b57fb8b766397a75ab8d7a977
berlyann15 <berlyan31@gmail.com>
2023-06-02 10:54:28 +0700

    update ke - 2

--- routes.js
+++ routes.js
@@ -1,25 +1,38 @@
 const routes = [
-    {
-        method: '*',
-        path: '/',
-        handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
-        },
+{
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+        return 'Halaman tidak dapat diakses dengan method tersebut';
     },
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Homepage';
-        },
+},
+{
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return 'Homepage';
     },
-    {
-        method: 'GET',
-        path: '/about',
-        handler: (request, h) => {
-            return 'About Page';
-        },
+},
+{
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+        return 'About Page';
     },
+},
 ];
 
-module.exports = routes;
+// Menambahkan versi di belakang response pada method GET
+const versionedRoutes = routes.map(route => {
+    if (route.method === 'GET') {
+        const originalHandler = route.handler;
+        route.handler = (request, h) => {
+            const originalResponse = originalHandler(request, h);
+            const version = 1; // Ubah angka versi sesuai kebutuhan
+            return originalResponse + ' - v' + version;
+        };
+    }
+    return route;
+});
+
+module.exports = versionedRoutes;
